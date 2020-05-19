750a3917ccbe4e69b59816c21254f48af2ccf812
CiocilteaRazvan <ciociltearazvan@gmail.com>
2020-05-19 08:16:41 +0300

    Added a few methods for use once the rest of the program is ready

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,9 +3,44 @@ import login.*;
 
 import java.util.concurrent.TimeUnit;
 
-public class Controller {
+public class Controller
+{
+    Login account;
+
+    private void printHelp()
+    {
+        System.out.print("You are logged in with an account of type: ");
+        if(account.isAdmin())
+        {
+            System.out.println("admin");
+        }
+        if(account.isUser())
+        {
+            System.out.println("user");
+        }
+        if(account.isGuest())
+        {
+            System.out.println("guest");
+        }
+
+        System.out.println("If you need help, type 'help'");
+    }
+
+    public static void clearScreen()
+    {
+        System. out. print("\033[H\033[2J");
+        System. out. flush();
+        printHelp();
+    }
+
+    private void readCommand()
+    {
+
+    }
+
     public static void main(String args[])
     {
+        Controller controller = new Controller;
         Login account = new Login();
         while(account.isLoggedIn() == false)
         {
@@ -19,6 +54,11 @@ public class Controller {
                 System.out.println("Error occured");
             }
         }
+
+        System.in.read(new byte[System.in.available()]);    // read any remaining data from terminal
+        clearScreen();
+
+
 
     }
 }
